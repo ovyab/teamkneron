3816c8daf11f7e5d84856835531c112a66bb6906
Ovya Barani <obarani@uci.edu>
2021-02-26 11:31:59 -0800

    revised augmentations + csv file

--- image_aug/augmentor.py
+++ image_aug/augmentor.py
@@ -1,49 +1,53 @@
-import os, scipy, cv2, imageio
+import os, scipy, imageio
 import imgaug as ia
 from imgaug import augmenters as iaa
 import numpy as np
+import pandas as pd
 
-ia.seed(1)
+def main():
 
-img = imageio.imread("original.jpg") #demo image
+    ia.seed(1)
+    numAugs = 4;
 
-images = np.array(
-    [img for _ in range(32)], dtype=np.uint8)
+    img = imageio.imread("original.jpg") #demo image
 
-  # creates 32 enhanced images using the methods below:
+    images = np.array(
+        [img for _ in range(numAugs)], dtype=np.uint8)
 
-seq = iaa.Sequential([
-    iaa.Fliplr(0.5), # horizontal flips
+    # creates 32 enhanced images using the methods below:
 
-    iaa.Crop(percent=(0, 0.1)), # random crops
+    seq = iaa.Sequential([
+        iaa.Fliplr(0.5), # horizontal flips
+        # motion blur with a kernel size of 15x15 pixels and a blur angle of either -45 or 45 degrees
+        iaa.Sometimes(0.5,
+            iaa.MotionBlur(k=15, angle=[-45, 45])
+        ),
+        # strengthen or weaken contrast
+        iaa.contrast.LinearContrast((0.75, 1.5)),
+        # gaussian noise
+        iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255), per_channel=0.5),
+        # make some images brighter, some darker
+        iaa.Multiply((0.8, 1.2), per_channel=0.2),
+        # randomly make some images super bright
+        iaa.Sometimes(0.2, iaa.imgcorruptlike.Brightness(severity=4)),
+        # affine transformations; scale/zoom, translate/move, rotate, and shear
+        iaa.Affine(
+            scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
+            translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
+            rotate=(-25, 25),
+            shear=(-8, 8)
+        )
+    ], name="img_augmentation", random_order=True) #apply augmenters in random order
 
-    # motion blur with a kernel size of 15x15 pixels and a blur angle of either -45 or 45 degrees
-    iaa.Sometimes(0.5,
-        iaa.MotionBlur(k=15, angle=[-45, 45])
-    ),
+    images_aug = seq.augment_images(images)
 
-    # strengthen or weaken contrast
-    iaa.contrast.LinearContrast((0.75, 1.5)),
+    for i in range(numAugs):
+        imageio.imwrite('/results/' + str(i)+'new.jpg', images_aug[i])  #write all changed images
 
-    # gaussian noise
-    iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255), per_channel=0.5),
+def test():
+    df_train = pd.read_csv('driver_imgs_list.csv', low_memory=True)
+    print('Number of samples in data set : {}'.format(df_train.shape[0]))
+    print('Number of classes : {}'.format(len((df_train.classname).unique())))
 
-    # make some images brighter, some darker
-    iaa.Multiply((0.8, 1.2), per_channel=0.2),
-
-    # randomly make some images super bright
-    iaa.Sometimes(0.2, iaa.imgcorruptlike.Brightness(severity=4)),
-
-    # affine transformations; scale/zoom, translate/move, rotate, and shear
-    iaa.Affine(
-        scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
-        translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
-        rotate=(-25, 25),
-        shear=(-8, 8)
-    )
-], random_order=True) #apply augmenters in random order
-
-images_aug = seq.augment_images(images)
-
-for i in range(32):
-    imageio.imwrite(str(i)+'new.jpg', images_aug[i])  #write all changed images
+if __name__ == "__main__":
+    test()
